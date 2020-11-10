5d3782d59980b1861e6018b36c652866d3bcf11f
Amanda <ti.amandamonteiro@gmail.com>
2020-11-10 11:59:40 -0300

    Simple Create form

---
+++ SaleWebMvc/SaleWebMvc/Services/SellerService.cs
@@ -0,0 +1,28 @@
+﻿using SaleWebMvc.Data;
+using SaleWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SaleWebMvcContext _context;//n altera valor
+
+        public SellerService(SaleWebMvcContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
