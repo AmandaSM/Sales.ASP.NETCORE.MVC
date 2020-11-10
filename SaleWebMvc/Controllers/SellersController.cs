4ff6aca7f7e86d0ea7981e2610016303e3b0a6d9
Amanda <ti.amandamonteiro@gmail.com>
2020-11-10 17:36:58 -0300

    Delete Seller

--- SaleWebMvc/Controllers/SellersController.cs
+++ SaleWebMvc/Controllers/SellersController.cs
@@ -33,11 +33,30 @@ namespace SaleWebMvc.Controllers
         //Prevenção ataque CSRF->Envio de dados maliciosos pela autenticação
         public IActionResult Create(Seller seller)
         {
-         
             _sellerService.Insert(seller);
-            return RedirectToAction(nameof(Index)); 
+            return RedirectToAction(nameof(Index));
             //name of->se mudar o nome do index n precisa mudar aqui
-
+        }
+        public IActionResult Delete(int? id)
+        {//?->opcional->nullable
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            //id.Value->por causa que pode ou n ter valor
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
